1f977394b992ff7bd2575b3661f5ffff319cc3dc
Denis Burdin <dionisiy.god.of.wine@gmail.com>
2022-12-01 00:19:19 +0200

    started marking of page

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector("gallery")
+for (const image of galleryItems) {
+    gallery.insertAdjacentHTML("beforeend", `
+
+<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>
+    `)
+}
+
+
